5d0a94d05525085b008a86bddc629d1a8ade2a59
Erin Gurley <erinmgurley@gmail.com>
2019-08-22 09:25:26 -0700

    successfully added route path to home search page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import Search from './containers/Search';
 
-export default class App extends Component {
-  render() {
-    return (
-      <>
-      <Search />
-      </>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <Route path="/" component={Search} />
+    </Router>
+  );
 }
+
+export default App; 
+
