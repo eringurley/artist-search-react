6898c940565ef00551d60a98d072bc20a9f51fd7
Erin Gurley <erinmgurley@gmail.com>
2019-08-22 14:14:45 -0700

    api changes

--- src/services/musicApi.js
+++ src/services/musicApi.js
@@ -1,33 +1,28 @@
 export const getArtists = (artists) => {
   return fetch(`http://musicbrainz.org/ws/2/artist?query=${artists}&fmt=json&limit=25`)
-    .then(res => ([res.ok, res.json()]))
-    .then(([ok, json]) => {
-      if(!ok) throw 'Unable to get artist';
-      return json;
+    .then(res => {
+      if(!res.ok) throw 'Could not get the artist.';
+      return console.log('Successfully retieved artists!'), res.json();
     });
 };
 
-// export const getSongs = songs => {
-//   return fetch('http://musicbrainz.org/ws/2/recording?release=<RELEASE_ID>&fmt=json')
-//     .then(res => ([res.ok, res.json()]))
-//     .then(([ok, json]) => {
-//       if(!ok) throw 'Unable to fetch quotes';
+export const getReleases = (id) => {
+  return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json`)
+    .then(res => {
+      if(!res.ok) throw 'Could not get the releases.';
+      return console.log('Successfully retieved releases!'), res.json();
+    });
+};
 
-//       return json;
-//     });
-// };
-
-// export const getReleases = releases => {
-//   return fetch('http://musicbrainz.org/ws/2/release?artist=<ARTIST_ID>&fmt=json')
-//     .then(res => ([res.ok, res.json()]))
-//     .then(([ok, json]) => {
-//       if(!ok) throw 'Unable to fetch quotes';
-
-//       return json;
-//     });
-// };
-// export const getCoverArt = coverArt => {
-//   return fetch('http://musicbrainz.org/ws/2/release?artist=<ARTIST_ID>&fmt=json')
+export const getSongs = (id) => {
+  return fetch(`http://musicbrainz.org/ws/2/recording?release=${id}&fmt=json`)
+    .then(res => {
+      if(!res.ok) throw 'Could not get the songs.';
+      return console.log('Successfully retieved songs!'), res.json();
+    });
+};
+// export const getCoverArt = (coverArt) => {
+//   return fetch(`http://coverartarchive.org/release/<RELEASE_ID>/front`)
 //     .then(res => ([res.ok, res.json()]))
 //     .then(([ok, json]) => {
 //       if(!ok) throw 'Unable to fetch quotes';
