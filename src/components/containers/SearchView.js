e139476a378f3bde59ba29958b3e75cde5e89081
Erin Gurley <erinmgurley@gmail.com>
2019-08-22 14:15:41 -0700

    renaming

---
+++ src/components/containers/SearchView.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { getArtists } from '../../services/musicApi';
+import Artists from '../artists/Artists';
+import Input from  '../Input';
+
+export default class SearchView extends Component {
+  state = {
+    artists: [],
+    artist: ''
+  }
+
+  inputChange = ({ target }) => {
+    this.setState({ artist: target.value });
+  }
+
+  getArtists = () => {
+    getArtists(this.state.artist)
+      .then(({ artists }) => {
+        this.setState({ artists });
+      });
+  }
+
+  onSearch = () => {
+    return this.getArtists();
+  }
+
+  render() {
+    return (
+    <>
+      <Input 
+        artist = {this.state.artist}
+        inputChange = {this.inputChange}
+        onSearch = {this.onSearch} 
+      />
+      <Artists
+        artists = {this.state.artists}
+      />
+    </>
+    );
+  }
+}
