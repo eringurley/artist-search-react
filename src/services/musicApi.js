7834c4a628c4c49fd09a785705d69a30793c7381
Erin Gurley <erinmgurley@gmail.com>
2019-08-23 14:23:41 -0700

    made some updates and edits to API and factored in for paging

--- src/services/musicApi.js
+++ src/services/musicApi.js
@@ -1,16 +1,29 @@
-export const getArtists = (artists) => {
-  return fetch(`http://musicbrainz.org/ws/2/artist?query=${artists}&fmt=json&limit=25`)
+const  NUMBER_PAGE_COUNT  = 25;
+
+const getTotalPages = count => Math.ceil(count / NUMBER_PER_PAGE);
+
+const request = (url, page) => {
+  const offset = (page - 1 ) * NUMBER_PER_PAGE;
+  return fetch(`${url}&limit=${NUMBER_PER_PAGE}&offset=${offset}`)
     .then(res => {
-      if(!res.ok) throw 'Could not get the artist.';
-      return console.log('Successfully retieved artists!'), res.json();
+      if(!res.ok) throw `Could not fetch ${url}`;
+      return res.json();
     });
+};
+
+export const getArtists = (artists, page = 1) => {
+  return request(`http://musicbrainz.org/ws/2/artist?query=${artists}&fmt=json&limit, page`)
+    .then(({ artists, count }) => ({
+      artists: artists.map(artist => ({ id: artist.id, name: artist.name, disambiguation: artist.disambiguation })),
+      totalPages: getTotalPages(count)
+    }));
 };
 
 export const getReleases = (id) => {
   return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json`)
     .then(res => {
       if(!res.ok) throw 'Could not get the releases.';
-      return console.log('Successfully retieved releases!'), res.json();
+      return console.log('Successfully retrieved releases!'), res.json();
     });
 };
 
@@ -18,14 +31,15 @@ export const getSongs = (id) => {
   return fetch(`http://musicbrainz.org/ws/2/recording?release=${id}&fmt=json`)
     .then(res => {
       if(!res.ok) throw 'Could not get the songs.';
-      return console.log('Successfully retieved songs!'), res.json();
+      return console.log('Successfully retrieved songs!'), res.json();
     });
 };
 
-export const getLyrics = (artistName, songTitle) => {
-  return fetch(`https://api.lyrics.ovh/v1/${artistName}/${songTitle}`)
+export const getLyrics = (artist, songTitle) => {
+  return fetch(`https://api.lyrics.ovh/v1/${artist}/${songTitle}`)
     .then(res => {
       if(!res.ok) throw 'Could not get the lyrics.';
-      return console.log('Successfully retieved lyrics!'), res.json();
+      console.log('api fires');
+      return res.json();
     });
 };
