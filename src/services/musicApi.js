82da3251f8e837e26108947ffc40d2c0c04540ef
Erin Gurley <erinmgurley@gmail.com>
2019-08-23 14:47:52 -0700

    got paging to work! edited number per page and page definition

--- src/services/musicApi.js
+++ src/services/musicApi.js
@@ -1,10 +1,10 @@
 const  NUMBER_PAGE_COUNT  = 25;
 
-const getTotalPages = count => Math.ceil(count / NUMBER_PER_PAGE);
+const getTotalPages = count => Math.ceil(count /  NUMBER_PAGE_COUNT);
 
 const request = (url, page) => {
-  const offset = (page - 1 ) * NUMBER_PER_PAGE;
-  return fetch(`${url}&limit=${NUMBER_PER_PAGE}&offset=${offset}`)
+  const offset = (page - 1) *  NUMBER_PAGE_COUNT;
+  return fetch(`${url}&limit=${ NUMBER_PAGE_COUNT }&offset=${offset}`)
     .then(res => {
       if(!res.ok) throw `Could not fetch ${url}`;
       return res.json();
@@ -12,23 +12,16 @@ const request = (url, page) => {
 };
 
 export const getArtists = (artists, page = 1) => {
-  return request(`http://musicbrainz.org/ws/2/artist?query=${artists}&fmt=json&limit, page`)
+  return request(`http://musicbrainz.org/ws/2/artist?query=${artists}&fmt=json&limit`, page)
     .then(({ artists, count }) => ({
       artists: artists.map(artist => ({ id: artist.id, name: artist.name, disambiguation: artist.disambiguation })),
       totalPages: getTotalPages(count)
     }));
 };
 
-export const getReleases = (id) => {
-  return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json`)
-    .then(res => {
-      if(!res.ok) throw 'Could not get the releases.';
-      return console.log('Successfully retrieved releases!'), res.json();
-    });
-};
 
-export const getSongs = (id) => {
-  return fetch(`http://musicbrainz.org/ws/2/recording?release=${id}&fmt=json`)
+export const getSongs = (id, page = 1) => {
+  return fetch(`http://musicbrainz.org/ws/2/recording?release=${id}&fmt=json`, page) 
     .then(res => {
       if(!res.ok) throw 'Could not get the songs.';
       return console.log('Successfully retrieved songs!'), res.json();
@@ -41,5 +34,23 @@ export const getLyrics = (artist, songTitle) => {
       if(!res.ok) throw 'Could not get the lyrics.';
       console.log('api fires');
       return res.json();
+    });
+};
+
+export const getReleases = (id, page = 1) => {
+  return request(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json`, page)
+    .then(json => {
+      const releases = json.releases.map(release => ({
+        id: release.id,
+        title: release.title,
+        coverUrl: release['cover-art-archive'].front ?
+          `http://coverartarchive.org/release/${release.id}/front-250` :
+          'https://via.placeholder.com/250'
+      }));
+
+      return {
+        releases,
+        totalPages: getTotalPages(json['release-count'])
+      };
     });
 };
