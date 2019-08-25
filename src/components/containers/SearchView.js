4af91cdcdc7c2f90615a32c0355ad1554c4c1087
Erin Gurley <erinmgurley@gmail.com>
2019-08-24 21:18:07 -0700

    added paging, rendering artist  to view, added  proptypes

--- src/components/containers/SearchView.js
+++ src/components/containers/SearchView.js
@@ -1,41 +1,71 @@
 import React, { Component } from 'react';
 import { getArtists } from '../../services/musicApi';
 import Artists from '../artists/Artists';
+import PropTypes from 'prop-types';
 import Input from  '../Input';
+import { Paging } from '../Paging';
 
-export default class SearchView extends Component {
+class SearchView extends Component {
+  static proptypes = {
+    page: PropTypes.number.isRequired,
+    setTotalPage: PropTypes.func.isRequied
+  }
+
   state = {
     artists: [],
-    artist: ''
+    artist: '',
+    error: null,
+    loading: false
+  }
+
+  fetch = () => {
+    getArtists(this.state.artist, this.props.page)
+      .then(({ artists, totalPages }) => {
+        this.setState({ artists, loading: false });
+        this.props.setTotalPages(totalPages);
+      });
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps.page !== this.props.page) { 
+      this.fetch();
+    }
   }
 
   inputChange = ({ target }) => {
     this.setState({ artist: target.value });
   }
 
-  getArtists = () => {
-    getArtists(this.state.artist)
-      .then(({ artists }) => {
-        this.setState({ artists });
-      });
-  }
-
-  onSearch = () => {
-    return this.getArtists();
+  onSearch = (event) => {
+    event.preventDefault();
+    this.fetch();
   }
 
   render() {
+    const {
+      artist,
+      artists,
+      loading,
+      error
+    } = this.state;
+
+    if(error) return <h1>Errrrror</h1>;
+    if(loading) return <h1>Looooooading</h1>;
+
     return (
     <>
       <Input 
-        artist = {this.state.artist}
+        artist = {artist}
         inputChange = {this.inputChange}
         onSearch = {this.onSearch} 
       />
       <Artists
-        artists = {this.state.artists}
+        artists = {artists}
       />
     </>
     );
   }
 }
+
+
+export default Paging(SearchView);
